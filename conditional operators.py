507c6b8bbd095f0151c9fdaadcf55492dd1d0a6e
Vani A S <vaniagasanakatti01@gmail.com>
2024-07-02 23:15:36 +0530

    Add files via upload

---
+++ conditional operators.py
@@ -0,0 +1,29 @@
+a=10
+b=20
+if a>b:
+    print('a is greater than b')
+elif b>a:
+    print('b is greater than a')
+else:
+    print('a equal to b')
+#TICKET    
+height=int(input('Enter height in feet:'))
+if(height>5):
+     print('Buy ticket')
+elif(height>3):
+    print('Buy half ticket')
+else:
+    print('No ticket required')
+    
+#GRADE
+result=int(input('percentage of marks'))
+if (result>70):
+    print('distinction')
+elif(result>60):
+    print('first class')
+elif(result>40):
+    print('second class')
+else:
+    print('pass')
+    
+            
