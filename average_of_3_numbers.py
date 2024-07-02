976ae0ef7e50d8ccf5c59642776dfa872d130978
Vani A S <vaniagasanakatti01@gmail.com>
2024-07-02 23:13:33 +0530

    Add files via upload

---
+++ average_of_3_numbers.py
@@ -0,0 +1,13 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #python program to calculate the avg of 3 numbers
+>>> a=50
+>>> b=80
+>>> c=100
+#if we want whole value use // 
+>>> d=(a+b+c//3)
+>>> print(d)
+#if we want decimal value use one /
+>>> e=(a+b+c)/3
+>>> print(e)
+ 
