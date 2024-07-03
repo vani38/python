5dcc506817836a03a50d4456426df643a685cff2
Vani A S <vaniagasanakatti01@gmail.com>
2024-07-03 22:27:05 +0530

    Delete even odd output.py

--- even odd output.py
+++
@@ -1,8 +0,0 @@
-Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:\Users\Administrator\AppData\Local\Programs\Python\Python312\even odd.py
-even numbers: [2, 4, 6, 8, 10, 12, 14]
-odd numbers: [1, 3, 5, 7, 9, 11, 13, 15]
-even numbers: 7
-odd numbers: 8
