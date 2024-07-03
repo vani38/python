0be06a431e922b82011060c07ad84f8864062425
Vani A S <vaniagasanakatti01@gmail.com>
2024-07-03 22:34:07 +0530

    Delete find and slice 1 output.py

--- find and slice 1 output.py
+++
@@ -1,7 +0,0 @@
-Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python312/find and slicing 1.py
-13
-18
-sita
