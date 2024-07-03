1eaafdfa55ce912a2c78563a34f82adc402a335c
Vani A S <vaniagasanakatti01@gmail.com>
2024-07-03 23:02:10 +0530

    Delete predefined module collections output.py

--- predefined module collections output.py
+++
@@ -1,9 +0,0 @@
-Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python312/predefined module collections.py
-Counter({'c': 4, 'a': 3, 'b': 3, 'd': 3, 'e': 2, 'f': 1})
-ChainMap({'x': 10, 'y': 20}, {'m': 30, 'n': 40})
-40
-10
-OrderedDict({'x': 1, 'y': 2, 'z': 3})
