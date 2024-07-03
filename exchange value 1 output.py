27d8b9bb83cdbb57989dc3b6c62e0538c11b9a96
Vani A S <vaniagasanakatti01@gmail.com>
2024-07-03 22:27:45 +0530

    Delete exchange value 1 output.py

--- exchange value 1 output.py
+++
@@ -1,8 +0,0 @@
-Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python312/exchange values 1.py
-20
-10
-50
-60
