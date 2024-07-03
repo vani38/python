6f62e21c93df3570d2f9aac2c9f58abcd1a309cf
Vani A S <vaniagasanakatti01@gmail.com>
2024-07-03 22:43:25 +0530

    Delete globals practice output.py

--- globals practice output.py
+++
@@ -1,9 +0,0 @@
-Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python312/globals practice.py
-140731928093432
-140731928093432
-75
-140731928091672
-20
